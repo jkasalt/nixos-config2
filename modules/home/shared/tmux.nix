5b96f5afec904fbd4732d6666c6c88ca78b21b01
Luca Bracone <lucabr123@gmail.com>
2025-04-08 23:32:46 +0200

    haha

---
+++ modules/home/shared/tmux.nix
@@ -0,0 +1,11 @@
+_: {
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    clock24 = true;
+    customPaneNavigationAndResize = true;
+    keyMode = "vi";
+    mouse = true;
+    sensibleOnTop = true;
+  };
+}
