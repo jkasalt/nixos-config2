78d1dc7501ac841e6dc36fa9622608b3eeac01a5
Luca Bracone <lucabr123@gmail.com>
2025-04-19 17:48:28 +0200

    refactor, add git config

--- hosts/juugas/users/lucab/home-configuration.nix
+++ hosts/juugas/users/lucab/home-configuration.nix
@@ -1,10 +1,39 @@
-{ flake, ... }:
+{ pkgs, flake, ... }:
 {
-  imports = [ flake.homeModules.shared ];
+  imports = builtins.attrValues {inherit (flake.homeModules) firefox git helix nushell shell-utils waybar tmux;};
 
   home = {
     username = "lucab";
     homeDirectory = "/home/lucab";
     stateVersion = "24.11";
+  };
+  home.packages = with pkgs; [
+    keepassxc
+    wine64
+    path-of-building
+    brave
+  ];
+
+  gtk.enable = true;
+
+  programs.home-manager.enable = true;
+
+  programs = {
+    kitty.enable = true;
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep 5 --keep-since 3d";
+    };
+    yazi.enable = true;
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/shell" = {
+        enabled-extensions = [ pkgs.gnomeExtensions.appindicator.extensionUuid ];
+      };
+    };
   };
 }
---
+++ modules/home/git.nix
@@ -0,0 +1,96 @@
+{pkgs, ...}: {
+  programs.git = {
+    enable = true;
+    delta.enable = true;
+    delta.options.side-by-side = true;
+
+    extraConfig = {
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      diff = {
+        colorMoved = "default";
+        algorithm = "histogram";
+        renames = "copies";
+      };
+      merge.conflictStyle = "diff3";
+      merge.tool = "nvimdiff4";
+
+      mergetool.nvimdiff4 = {
+        cmd = "nvim -d $LOCAL $BASE $REMOTE $MERGED -c '$wincmd w' -c 'wincmd J'";
+      };
+
+      rerere.enabled = true;
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      init.defaultBranch = "main";
+    };
+
+    maintenance.enable = true;
+
+    # Thank you: https://github.com/fufexan/dotfiles/blob/main/home/terminal/programs/git.nix
+    aliases = let
+      log = "log --show-notes='*' --abbrev-commit --pretty=format:'%Cred%h %Cgreen(%aD)%Creset -%C(bold red)%d%Creset %s %C(bold blue)<%an>% %Creset' --graph";
+    in {
+      a = "add --patch"; # make it a habit to consciosly add hunks
+      ad = "add";
+
+      b = "branch";
+      ba = "branch -a"; # list remote branches
+      bd = "branch --delete";
+      bdd = "branch -D";
+
+      c = "commit";
+      ca = "commit --amend";
+      cm = "commit --message";
+
+      cl = "clone";
+
+      d = "diff";
+      ds = "diff --staged";
+
+      h0 = "show";
+      h1 = "show HEAD^";
+      h2 = "show HEAD^^";
+      h3 = "show HEAD^^^";
+      h4 = "show HEAD^^^^";
+      h5 = "show HEAD^^^^^";
+
+      m = "merge";
+      ma = "merge --abort";
+      mc = "merge --continue";
+
+      p = "push";
+      pf = "push --force-with-lease";
+
+      pl = "pull";
+
+      l = log;
+      lp = "${log} --patch";
+      la = "${log} --all";
+
+      rs = "restore";
+      rsp = ''
+        !f() { git restore --source="$1" --patch "$2"; }; f;
+      '';
+      rsh = "reset --hard";
+
+      sw = "switch";
+      sc = "switch -c";
+
+      s = "status --short --branch";
+      ss = "status";
+
+      st = "stash";
+      stc = "stash clear";
+      sth = "stash show --patch";
+      stl = "stash list";
+      stp = "stash pop";
+
+      forgor = "commit --amend --no-edit";
+      oops = "restore";
+    };
+
+    userEmail = "lucabr123@gmail.com";
+    userName = "Luca Bracone";
+  };
+}
