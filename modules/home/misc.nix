d1a0ee90ce0eef01ef4596d36ea8c59695bbb49d
Luca Bracone <lucabr123@gmail.com>
2025-04-30 22:44:34 +0200

    add gitui

--- modules/home/misc.nix
+++ modules/home/misc.nix
@@ -7,6 +7,7 @@
         notebook.dir = "~/notebook";
       };
     };
+
     mpv = {
       enable = true;
 
@@ -28,6 +29,24 @@
         ytdl-format = "bestvideo+bestaudio";
         cache-default = 4000000;
       };
+    };
+
+    gitui = {
+      enable = true;
+      keyConfig = # ron
+      ''
+	(
+        move_left: Some(( code: Char('h'), modifiers: "")),
+        move_right: Some(( code: Char('l'), modifiers: "")),
+        move_up: Some(( code: Char('k'), modifiers: "")),
+        move_down: Some(( code: Char('j'), modifiers: "")),
+
+        stash_open: Some(( code: Char('l'), modifiers: "")),
+        open_help: Some(( code: F(1), modifiers: "")),
+
+        status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
+	)
+      '';
     };
   };
 }
