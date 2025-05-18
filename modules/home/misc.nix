c8c576b6a71b7e256385acb5030243bb5d686f86
Luca Bracone <lucabr123@gmail.com>
2025-05-18 13:09:45 +0200

    add anki

--- modules/home/misc.nix
+++ modules/home/misc.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [htop];
+  home.packages = with pkgs; [
+    htop
+    anki-bin
+  ];
   programs = {
     zk = {
       enable = true;
@@ -38,17 +41,17 @@
       enable = true;
       keyConfig = # ron
         ''
-	(
-	  move_left: Some(( code: Char('h'), modifiers: "")),
-	  move_right: Some(( code: Char('l'), modifiers: "")),
-	  move_up: Some(( code: Char('k'), modifiers: "")),
-	  move_down: Some(( code: Char('j'), modifiers: "")),
+          	(
+          	  move_left: Some(( code: Char('h'), modifiers: "")),
+          	  move_right: Some(( code: Char('l'), modifiers: "")),
+          	  move_up: Some(( code: Char('k'), modifiers: "")),
+          	  move_down: Some(( code: Char('j'), modifiers: "")),
 
-	  stash_open: Some(( code: Char('l'), modifiers: "")),
-	  open_help: Some(( code: F(1), modifiers: "")),
+          	  stash_open: Some(( code: Char('l'), modifiers: "")),
+          	  open_help: Some(( code: F(1), modifiers: "")),
 
-	  status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
-	)
+          	  status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
+          	)
         '';
     };
   };
