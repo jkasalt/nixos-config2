2595c9bd77ea3091a50c86a3cb7777a5d755e8fa
Luca Bracone <lucabr123@gmail.com>
2025-05-18 13:07:36 +0200

    whitspace

--- modules/home/misc.nix
+++ modules/home/misc.nix
@@ -38,17 +38,17 @@
       enable = true;
       keyConfig = # ron
         ''
-          	(
-                  move_left: Some(( code: Char('h'), modifiers: "")),
-                  move_right: Some(( code: Char('l'), modifiers: "")),
-                  move_up: Some(( code: Char('k'), modifiers: "")),
-                  move_down: Some(( code: Char('j'), modifiers: "")),
+	(
+	  move_left: Some(( code: Char('h'), modifiers: "")),
+	  move_right: Some(( code: Char('l'), modifiers: "")),
+	  move_up: Some(( code: Char('k'), modifiers: "")),
+	  move_down: Some(( code: Char('j'), modifiers: "")),
 
-                  stash_open: Some(( code: Char('l'), modifiers: "")),
-                  open_help: Some(( code: F(1), modifiers: "")),
+	  stash_open: Some(( code: Char('l'), modifiers: "")),
+	  open_help: Some(( code: F(1), modifiers: "")),
 
-                  status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
-          	)
+	  status_reset_item: Some(( code: Char('U'), modifiers: "SHIFT")),
+	)
         '';
     };
   };
