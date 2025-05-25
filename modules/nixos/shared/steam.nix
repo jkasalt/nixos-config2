4420de655a6421f23a5a193b4bae14e4db40d9a6
Luca Bracone <lucabr123@gmail.com>
2025-05-25 12:19:48 +0200

    add proton-ge

--- modules/nixos/shared/steam.nix
+++ modules/nixos/shared/steam.nix
@@ -1,7 +1,9 @@
-_: {
+{ pkgs, ... }:
+{
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
   programs.gamemode.enable = true;
