ca6d02c94a667ecc8e0bcb46d985eebffcad0af0
Luca Bracone <lucabr123@gmail.com>
2025-09-27 16:59:26 +0200

    add wl-clipboard to gnome

--- modules/nixos/shared/gnome.nix
+++ modules/nixos/shared/gnome.nix
@@ -1,4 +1,6 @@
-_: {
+{ pkgs, ... }:
+{
+
   services = {
     xserver = {
       enable = true;
@@ -7,5 +9,7 @@ _: {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+
+  environment.systemPackages = [ pkgs.wl-clipboard ];
   programs.xwayland.enable = true;
 }
