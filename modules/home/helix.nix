5b2f8bbe438d27495caacf84b71af45ebf292947
Luca Bracone <lucabr123@gmail.com>
2026-01-22 22:19:41 +0100

    nixfmt change

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -33,7 +33,7 @@
       {
         name = "nix";
         auto-format = true;
-        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        formatter.command = lib.getExe pkgs.nixfmt;
       }
     ];
   };
