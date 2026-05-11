42ad51e30c3c0183df049fbca0721a59be57651b
Luca Bracone <lucabr123@gmail.com>
2026-05-11 21:54:04 +0200

    format

--- flake-modules/features/programs/emacs.nix
+++ flake-modules/features/programs/emacs.nix
@@ -1,22 +1,24 @@
 {
-  flake.modules.homeManager.emacs = {pkgs, ...}: {
-    programs.emacs = {
-      enable = true;
-      package = pkgs.emacs-pgtk;
-      extraPackages = epkgs: [
-        epkgs.vterm
-        (epkgs.treesit-grammars.with-grammars (g: [
-          g.tree-sitter-nix
-          g.tree-sitter-haskell
-          g.tree-sitter-bash
-        ]))
+  flake.modules.homeManager.emacs =
+    { pkgs, ... }:
+    {
+      programs.emacs = {
+        enable = true;
+        package = pkgs.emacs-pgtk;
+        extraPackages = epkgs: [
+          epkgs.vterm
+          (epkgs.treesit-grammars.with-grammars (g: [
+            g.tree-sitter-nix
+            g.tree-sitter-haskell
+            g.tree-sitter-bash
+          ]))
+        ];
+      };
+      services.emacs.enable = true;
+      home.packages = with pkgs; [
+        ripgrep
+        nil
+        gcc # for tree-sitter
       ];
     };
-    services.emacs.enable = true;
-    home.packages = with pkgs; [
-      ripgrep
-      nil
-      gcc # for tree-sitter
-    ];
-  };
 }
