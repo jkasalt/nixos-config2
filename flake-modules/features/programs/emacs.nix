77bb683c36a9a961208b90c0526561d108ff7d7b
Luca Bracone <lucabr123@gmail.com>
2026-05-16 14:31:51 +0200

    rm git-sync use sync.el plugin instead

--- flake-modules/features/programs/emacs.nix
+++ flake-modules/features/programs/emacs.nix
@@ -14,36 +14,8 @@
       home.packages = with pkgs; [
         ripgrep
         nil # nix lsp
-        git-sync # for org-mode sync
         cmigemo # for searching japanese buffers with romaji
         mozc-ut # for japanese input
       ];
-      systemd.user = {
-        services.git-sync-org = {
-          Unit = {
-            Description = "Performs git sync in the org directory";
-          };
-          Service = {
-            Type = "simple";
-            ExecStart = pkgs.writeShellScript "org-sync" ''
-              cd ~/org || exit 1
-              ${lib.getExe' pkgs.git-sync "git-sync"}
-            '';
-            Nice = 19;
-          };
-        };
-        timers.git-sync-org = {
-          Unit = {
-            Description = "Performs git sync in the org directory";
-          };
-          Timer = {
-            OnBootsec = "1min";
-            OnUnitActiveSec = "5min";
-          };
-          Install = {
-            WantedBy = [ "timers.target" ];
-          };
-        };
-      };
     };
 }
