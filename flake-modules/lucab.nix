5f00f25be94c954e3e25c734549ef5d6273aa520
Luca Bracone <lucabr123@gmail.com>
2026-07-02 08:32:38 +0900

    niri: rewrite compositor config as home-manager module
    Drop the KDL file and wrapper-modules-based niri package in favour of
    the sodiboo niri flake's home-manager module with inline Nix settings.

--- flake-modules/lucab.nix
+++ flake-modules/lucab.nix
@@ -35,9 +35,11 @@ in {
             ai
             anki
             email
+            niri
             ;
         }
         ++ [
+          inputs.niri.homeModules.config
           {
             home.username = "lucab";
             home.homeDirectory = "/home/lucab";
